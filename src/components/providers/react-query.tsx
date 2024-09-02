8aeb67398626662aa5b487bcca77e695cb9438aa
Ayer <gabrielcbay@outlook.com>
2024-09-01 21:47:00 -0300

    Create react-query.tsx

---
+++ src/components/providers/react-query.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { useState } from "react"
+
+function ReactQueryProvider({ children }: React.PropsWithChildren) {
+  const [client] = useState(new QueryClient())
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+export default ReactQueryProvider;
